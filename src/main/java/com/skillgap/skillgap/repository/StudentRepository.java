a72df5a6ea13861877f1005586b84edda34f8523
Sri Vasu <pvasu9055@gmail.com>
2026-03-09 11:55:27 +0530

    fix student repository

--- src/main/java/com/skillgap/skillgap/repository/StudentRepository.java
+++ src/main/java/com/skillgap/skillgap/repository/StudentRepository.java
@@ -1,9 +1,9 @@
 package com.skillgap.skillgap.repository;
 
+import com.skillgap.skillgap.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.skillgap.skillgap.entity.Student;
-
+ 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
 
